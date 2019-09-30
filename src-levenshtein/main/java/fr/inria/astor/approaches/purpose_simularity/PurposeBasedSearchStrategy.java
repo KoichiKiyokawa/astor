7096255bce3e5c391511771432f07665f19519fa
K.Kiyokawa <koichi20110068@gmail.com>
2019-09-30 11:22:51 +0900

    temporary comment out deeplearning4j to save time of compile

--- src-levenshtein/main/java/fr/inria/astor/approaches/purpose_simularity/PurposeBasedSearchStrategy.java
+++ src-levenshtein/main/java/fr/inria/astor/approaches/purpose_simularity/PurposeBasedSearchStrategy.java
@@ -14,13 +14,13 @@ import fr.inria.astor.core.solutionsearch.spaces.ingredients.IngredientPool;
 import fr.inria.astor.core.solutionsearch.spaces.ingredients.IngredientSearchStrategy;
 import fr.inria.astor.approaches.jgenprog.operators.ReplaceOp;
 
-import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
-import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
-import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
-import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
-import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.ops.transforms.Transforms;
+// import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+// import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
+// import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+// import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+// import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+// import org.nd4j.linalg.api.ndarray.INDArray;
+// import org.nd4j.linalg.ops.transforms.Transforms;
 
 public class PurposeBasedSearchStrategy extends IngredientSearchStrategy {
 	public String originalProjectRootDir;
@@ -47,30 +47,30 @@ public class PurposeBasedSearchStrategy extends IngredientSearchStrategy {
 			baseElem.setCommitMessage(this.originalProjectRootDir);
 		}
 		// baseElementsをコミットメッセージに応じて並び替える
-		try {
-      ParagraphVectors vec = WordVectorSerializer.readParagraphVectors(getSavedModelFile());
-      TokenizerFactory t = new DefaultTokenizerFactory();
-      t.setTokenPreProcessor(new CommonPreprocessor());
-      vec.setTokenizerFactory(t);
-      // コサイン類似度を測定
-			// caution! モデルの中に存在していない文章は無理
-			Collections.sort(baseElements, new Comparator<Ingredient>() {
-				@Override
-				public int compare(Ingredient ingredientA, Ingredient ingredientB) {
-					INDArray vecIngredientA_CommitMessage = vec.inferVector(ingredientA.commitMessage);
-					INDArray vecIngredientB_CommitMessage = vec.inferVector(ingredientB.commitMessage);
-					INDArray vecModificationPointCommitMessage = vec.inferVector(modificationPoint.commitMessage);
-					double simA2modif = Transforms.cosineSim(vecIngredientA_CommitMessage, vecModificationPointCommitMessage);
-					double simB2modif = Transforms.cosineSim(vecIngredientB_CommitMessage, vecModificationPointCommitMessage);
+		// try {
+    //   ParagraphVectors vec = WordVectorSerializer.readParagraphVectors(getSavedModelFile());
+    //   TokenizerFactory t = new DefaultTokenizerFactory();
+    //   t.setTokenPreProcessor(new CommonPreprocessor());
+    //   vec.setTokenizerFactory(t);
+    //   // コサイン類似度を測定
+		// 	// caution! モデルの中に存在していない文章は無理
+		// 	Collections.sort(baseElements, new Comparator<Ingredient>() {
+		// 		@Override
+		// 		public int compare(Ingredient ingredientA, Ingredient ingredientB) {
+		// 			INDArray vecIngredientA_CommitMessage = vec.inferVector(ingredientA.commitMessage);
+		// 			INDArray vecIngredientB_CommitMessage = vec.inferVector(ingredientB.commitMessage);
+		// 			INDArray vecModificationPointCommitMessage = vec.inferVector(modificationPoint.commitMessage);
+		// 			double simA2modif = Transforms.cosineSim(vecIngredientA_CommitMessage, vecModificationPointCommitMessage);
+		// 			double simB2modif = Transforms.cosineSim(vecIngredientB_CommitMessage, vecModificationPointCommitMessage);
 
-					return Double.compare(simA2modif, simB2modif);
-				}
-			});
-			// end sort
+		// 			return Double.compare(simA2modif, simB2modif);
+		// 		}
+		// 	});
+		// 	// end sort
 
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    // } catch (IOException e) {
+    //   e.printStackTrace();
+    // }
 
 
 		return null;
