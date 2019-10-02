d31d7710f8025420f01f45825a5d05416922c52a
K.Kiyokawa <koichi20110068@gmail.com>
2019-10-02 11:43:02 +0900

    Add abstract class HasCommitMessage

---
+++ src/main/java/fr/inria/astor/core/entities/HasCommitMessage.java
@@ -0,0 +1,75 @@
+package fr.inria.astor.core.entities;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+import spoon.reflect.declaration.CtElement;
+
+import fr.inria.astor.approaches.purpose_simularity.entities.Commit;
+import fr.inria.astor.util.CommandExecuter;
+
+public abstract class HasCommitMessage {
+
+  private String originalProjectRootDir;
+  private String javaFilePath;
+
+  public String commitMessage = "";
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+
+  public abstract CtElement getCodeElement();
+
+  // by `git log -L`
+	public void setCommitMessage(String originalProjectRootDir, String javaFilePath) {
+    this.originalProjectRootDir = originalProjectRootDir;
+    this.javaFilePath = javaFilePath;
+
+		int lineNumber = this.getCodeElement().getPosition().getLine();
+		String[] args = { "git", "log", "-L",
+				String.format("%d,%d:%s", lineNumber, lineNumber, javaFilePath + "/" + getRelativeFilePath()) };
+		String res = CommandExecuter.run(args, originalProjectRootDir);
+		log.info("line number: " + lineNumber);
+		log.info("git result: " + res);
+		parseGitLogLAndSetCommitMessage();
+  }
+
+  private String getRelativeFilePath() {
+		String rawPath = this.getCodeElement().getPath().toString();
+
+		List<String> paths = new ArrayList<>();
+		String fileName = "";
+
+		for (String str : rawPath.split("#")) {
+			if (str.startsWith("subPackage[name=")) {
+				paths.add(str.substring("subPackage[name=".length(), str.length() - 1));
+			}
+
+			if (str.startsWith("containedType[name=")) {
+				fileName = str.substring("containedType[name=".length(), str.length() - 1);
+			}
+		}
+
+		String res = String.format("%s/%s.java", String.join("/", paths), fileName);
+		log.info("get relative file path: " + res);
+
+		return res;
+	}
+
+	private void parseGitLogLAndSetCommitMessageAndSetCommitMessage(String gitLog) {
+		// Defects4jが余計なコミットを3つ追加することがあるので、直近4つ分のコミットを記録
+		List<Commit> commits = new ArrayList<>();
+		for (String line:gitLog.split("\n")) {
+			if (line.startsWith("commit ")) {
+				String commitSHA =line.substring("commit ".length());
+				commits.add(new Commit(commitSHA, this.originalProjectRootDir));
+			}
+		}
+
+		if (commits.get(0).isCommitedByDefects4j()){
+			this.commitMessage = comimts.get(3).getMessage();
+		}else{
+			this.commitMessage = commits.get(0).getMessage();
+		}
+	}
+}
--- src/main/java/fr/inria/astor/core/entities/ModificationPoint.java
+++ src/main/java/fr/inria/astor/core/entities/ModificationPoint.java
@@ -6,11 +6,10 @@ import java.io.File;
 import java.io.IOException;
 
 import fr.inria.astor.util.CommandExecuter;
+import fr.inria.astor.core.entities.HasCommitMessage;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtVariable;
-
-import org.apache.log4j.Logger;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.blame.BlameResult;
@@ -26,7 +25,7 @@ import org.eclipse.jgit.api.errors.GitAPIException;
  * @author Matias Martinez, matias.martinez@inria.fr
  *
  */
-public class ModificationPoint implements Comparable {
+public class ModificationPoint extends HasCommitMessage implements Comparable {
 
 	protected ProgramVariant programVariant;
 
@@ -39,10 +38,6 @@ public class ModificationPoint implements Comparable {
 	public int identified = 0;
 
 	protected int generation = -1;
-
-	/** Added */
-	public String commitMessage = "";
-	protected Logger log = Logger.getLogger(this.getClass().getName());
 
 	public ModificationPoint() {
 	}
@@ -112,51 +107,6 @@ public class ModificationPoint implements Comparable {
 
 	public ModificationPoint clone() {
 		return new ModificationPoint(identified, codeElement, ctClass, contextOfModificationPoint, this.generation);
-	}
-
-	// by `git log -L`
-	private void setCommitMessage(String originalProjectRootDir, String javaFilePath) {
-		int lineNumber = this.getCodeElement().getPosition().getLine();
-		String[] args = { "git", "log", "-L",
-				String.format("%d,%d:%s", lineNumber, lineNumber, javaFilePath + "/" + getRelativeFilePath()) };
-		String res = CommandExecuter.run(args, originalProjectRootDir);
-		log.info("line number: " + lineNumber);
-		log.info("git result: " + res);
-		// TODO: 結果をパースする
-	}
-
-	private String getRelativeFilePath() {
-		String rawPath = this.getCodeElement().getPath().toString();
-
-		List<String> paths = new ArrayList<>();
-		String fileName = "";
-
-		for (String str : rawPath.split("#")) {
-			if (str.startsWith("subPackage[name=")) {
-				paths.add(str.substring("subPackage[name=".length(), str.length() - 1));
-			}
-
-			if (str.startsWith("containedType[name=")) {
-				fileName = str.substring("containedType[name=".length(), str.length() - 1);
-			}
-		}
-
-		String res = String.format("%s/%s.java", String.join("/", paths), fileName);
-		log.info("get relative file path: " + res);
-
-		return res;
-	}
-
-	private parseGitLogL(String gitLog) {
-		// Defects4jが余計なコミットを3つ追加することがあるので、直近4つ分のコミットを記録
-		List<String> commitSHAs = new ArrayList<>();
-		for (String line:gitLog.split("\n")) {
-			if (line.startWith("commit ")) {
-				commitSHAs.add(line.substring("commit ".length()));
-			}
-		}
-
-		
 	}
 
 }
