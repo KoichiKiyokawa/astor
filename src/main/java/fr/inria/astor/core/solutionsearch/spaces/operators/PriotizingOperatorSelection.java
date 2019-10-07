d47feaf7a8d6c6603e65424593fe5224ae4048ad
K.Kiyokawa <koichi20110068@gmail.com>
2019-10-07 14:07:22 +0900

    Fix PriotizingOperatorSelection class

--- src/main/java/fr/inria/astor/core/solutionsearch/spaces/operators/PriotizingOperatorSelection.java
+++ src/main/java/fr/inria/astor/core/solutionsearch/spaces/operators/PriotizingOperatorSelection.java
@@ -1,8 +1,14 @@
 package fr.inria.astor.core.solutionsearch.spaces.operators;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
-public class PriotizingOperatorSelection extends OperatorSelectionStrategy{
+import fr.inria.astor.core.entities.SuspiciousModificationPoint;
+import fr.inria.astor.core.entities.WeightElement;
+
+public class PriotizingOperatorSelection extends OperatorSelectionStrategy {
 
   protected static Logger log = Logger.getLogger(PriotizingOperatorSelection.class.getName());
 
@@ -11,10 +17,25 @@ public class PriotizingOperatorSelection extends OperatorSelectionStrategy{
   public PriotizingOperatorSelection(OperatorSpace space) {
     super(space);
     // for
-    for (AstorOperator op : this.operatorSpace.getOperators()){
-    //  if (op instanceof )
-    //   weightedOperators.add(new WeightElement<>(op, uniformprobability));
+    for (AstorOperator op : this.operatorSpace.getOperators()) {
+      // if (op instanceof )
+      // weightedOperators.add(new WeightElement<>(op, uniformprobability));
     }
-   }
+  }
+
+  // 一旦コピペ
+  @Override
+  public AstorOperator getNextOperator() {
+
+    WeightElement<?> selected = WeightElement.selectElementWeightBalanced(this.weightedOperators);
+    AstorOperator selectedOp = (AstorOperator) selected.element;
+    return selectedOp;
+  }
+
+  @Override
+  public AstorOperator getNextOperator(SuspiciousModificationPoint modificationPoint) {
+    return getNextOperator();
+  }
+  // 一旦コピペ ここまで
 
 }
