f5927093847840dc0764a0afd2062caef0a55994
K.Kiyokawa <koichi20110068@gmail.com>
2019-12-03 17:43:14 +0900

    style: indent size

--- src-levenshtein/main/java/fr/inria/astor/approaches/purpose_simularity/PurposeBasedSearchStrategy.java
+++ src-levenshtein/main/java/fr/inria/astor/approaches/purpose_simularity/PurposeBasedSearchStrategy.java
@@ -27,149 +27,146 @@ import org.nd4j.linalg.ops.transforms.Transforms;
 
 public class PurposeBasedSearchStrategy extends IngredientSearchStrategy {
 
-	private String javaFilePath;
-	public String originalProjectRootDir;
+  private String javaFilePath;
+  public String originalProjectRootDir;
 
-	private List<CtElement> locationsAnalyzed = new ArrayList<>();
+  private List<CtElement> locationsAnalyzed = new ArrayList<>();
 
-	protected org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass().getName());
+  protected org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass().getName());
 
-	public PurposeBasedSearchStrategy(IngredientPool space, String originalProjectRootDir, String javaFilePath) {
-		super(space);
-		this.javaFilePath = javaFilePath;
-		this.originalProjectRootDir = originalProjectRootDir;
-	}
+  public PurposeBasedSearchStrategy(IngredientPool space, String originalProjectRootDir, String javaFilePath) {
+    super(space);
+    this.javaFilePath = javaFilePath;
+    this.originalProjectRootDir = originalProjectRootDir;
+  }
 
-	private File getSavedModelFile() {
-		// ex) Math-2 -> math
-		String id = ConfigurationProperties.getProperty("projectIdentifier").split("-")[0].toLowerCase();
-		return new File(String.format("/astor/models/%s.txt", id));
-	}
+  private File getSavedModelFile() {
+    // ex) Math-2 -> math
+    String id = ConfigurationProperties.getProperty("projectIdentifier").split("-")[0].toLowerCase();
+    return new File(String.format("/astor/models/%s.txt", id));
+  }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public Ingredient getFixIngredient(ModificationPoint modificationPoint, AstorOperator operationType) {
+  @SuppressWarnings("unchecked")
+  @Override
+  public Ingredient getFixIngredient(ModificationPoint modificationPoint, AstorOperator operationType) {
 
-		modificationPoint.setCommitMessage(this.originalProjectRootDir, this.javaFilePath);
+    modificationPoint.setCommitMessage(this.originalProjectRootDir, this.javaFilePath);
 
-		List<Ingredient> baseElements = getIngredientsFromSpace(modificationPoint, operationType);
+    List<Ingredient> baseElements = getIngredientsFromSpace(modificationPoint, operationType);
 
-		if (baseElements == null || baseElements.isEmpty()) {
-			return null;
-		}
+    if (baseElements == null || baseElements.isEmpty()) {
+      return null;
+    }
 
-		for (HasCommitMessage elem : baseElements) {
-			elem.setCommitMessage(this.originalProjectRootDir, this.javaFilePath);
-		}
+    for (HasCommitMessage elem : baseElements) {
+      elem.setCommitMessage(this.originalProjectRootDir, this.javaFilePath);
+    }
 
-		// We store the location to avoid sorting the ingredient twice.
-		if (!locationsAnalyzed.contains(modificationPoint.getCodeElement())) {
-			locationsAnalyzed.add(modificationPoint.getCodeElement());
+    // We store the location to avoid sorting the ingredient twice.
+    if (!locationsAnalyzed.contains(modificationPoint.getCodeElement())) {
+      locationsAnalyzed.add(modificationPoint.getCodeElement());
 
-			// We have never analyze this location, let's sort the ingredients.
-			try {
-				ParagraphVectors vec = WordVectorSerializer.readParagraphVectors(getSavedModelFile());
-				TokenizerFactory t = new DefaultTokenizerFactory();
-				t.setTokenPreProcessor(new CommonPreprocessor());
-				vec.setTokenizerFactory(t);
-				Collections.sort(baseElements, new Comparator<Ingredient>() {
-					@Override
-					public int compare(Ingredient ingredientA, Ingredient ingredientB) {
+      // We have never analyze this location, let's sort the ingredients.
+      try {
+        ParagraphVectors vec = WordVectorSerializer.readParagraphVectors(getSavedModelFile());
+        TokenizerFactory t = new DefaultTokenizerFactory();
+        t.setTokenPreProcessor(new CommonPreprocessor());
+        vec.setTokenizerFactory(t);
+        Collections.sort(baseElements, new Comparator<Ingredient>() {
+          @Override
+          public int compare(Ingredient ingredientA, Ingredient ingredientB) {
 
-						if (ingredientA.commitMessage.length() == 0) {
-							if (ingredientB.commitMessage.length() == 0) {
-								return 0;
-							}
-							// ingredientAのコミットメッセージが空かつ、
-							// ingredientBのコミットメッセージが空だったら、Aをうしろに
-							return 1;
-						}
+            if (ingredientA.commitMessage.length() == 0) {
+              if (ingredientB.commitMessage.length() == 0) {
+                return 0;
+              }
+              // ingredientAのコミットメッセージが空かつ、
+              // ingredientBのコミットメッセージが空だったら、Aをうしろに
+              return 1;
+            }
 
-						// 同じコミットメッセージのものは優先して使わない
-						if (modificationPoint.commitMessage.equals(ingredientA.commitMessage)) {
-							if (modificationPoint.commitMessage.equals(ingredientB.commitMessage)) {
-								// ingredientAもingredientBもmodificationPointと同じコミットメッセージだったら、
-								// 一致していると定義
-								return 0;
-							} else {
-								// modificationPointとingredientAのコミットメッセージが同じ かつ
-								// modificationPointとingredientBのコミットメッセージが違う とき、
-								// ingredientAをうしろに
-								return 1;
-							}
-						} else {
-							if (modificationPoint.commitMessage.equals(ingredientB.commitMessage)) {
-								// modificationPointとingredientAのコミットメッセージが違う かつ
-								// modificationPointとingredientBのコミットメッセージが同じ とき、
-								// そのまま
-								return -1;
-							}
-						}
+            // 同じコミットメッセージのものは優先して使わない
+            if (modificationPoint.commitMessage.equals(ingredientA.commitMessage)) {
+              if (modificationPoint.commitMessage.equals(ingredientB.commitMessage)) {
+                // ingredientAもingredientBもmodificationPointと同じコミットメッセージだったら、
+                // 一致していると定義
+                return 0;
+              } else {
+                // modificationPointとingredientAのコミットメッセージが同じ かつ
+                // modificationPointとingredientBのコミットメッセージが違う とき、
+                // ingredientAをうしろに
+                return 1;
+              }
+            } else {
+              if (modificationPoint.commitMessage.equals(ingredientB.commitMessage)) {
+                // modificationPointとingredientAのコミットメッセージが違う かつ
+                // modificationPointとingredientBのコミットメッセージが同じ とき、
+                // そのまま
+                return -1;
+              }
+            }
 
-						// modificationPointとingredientAのコミットメッセージが違う かつ
-						// modificationPointとingredientBのコミットメッセージが違う とき、
-						// コサイン類似度で並び替える
+            // modificationPointとingredientAのコミットメッセージが違う かつ
+            // modificationPointとingredientBのコミットメッセージが違う とき、
+            // コサイン類似度で並び替える
 
-						// コサイン類似度を測定
-						// caution! モデルの中に存在していない文章は無理
-						try {
-							INDArray vecingredientA_CommitMessage = vec.inferVector(ingredientA.commitMessage);
-							INDArray vecingredientB_CommitMessage = vec.inferVector(ingredientB.commitMessage);
-							INDArray vecModifCommitMessage = vec.inferVector(modificationPoint.commitMessage);
-							double simA2modif = Transforms.cosineSim(vecingredientA_CommitMessage,
-									vecModifCommitMessage);
+            // コサイン類似度を測定
+            // caution! モデルの中に存在していない文章は無理
+            try {
+              INDArray vecingredientA_CommitMessage = vec.inferVector(ingredientA.commitMessage);
+              INDArray vecingredientB_CommitMessage = vec.inferVector(ingredientB.commitMessage);
+              INDArray vecModifCommitMessage = vec.inferVector(modificationPoint.commitMessage);
+              double simA2modif = Transforms.cosineSim(vecingredientA_CommitMessage, vecModifCommitMessage);
 
-							double simB2modif = Transforms.cosineSim(vecingredientB_CommitMessage,
-									vecModifCommitMessage);
+              double simB2modif = Transforms.cosineSim(vecingredientB_CommitMessage, vecModifCommitMessage);
 
-							return -1 * Double.compare(simA2modif, simB2modif);
-						} catch (org.nd4j.linalg.exception.ND4JIllegalStateException e) {
-							// モデル内にコミットメッセージが存在しなかったとき
-							log.info(String.format(
-									"model does not have the commit message. IngredientA: %s, IngredientB: %s",
-									ingredientA.commitMessage, ingredientB.commitMessage));
-						}
-						return 0; // default
-					}
-				});
-				// end sort
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+              return -1 * Double.compare(simA2modif, simB2modif);
+            } catch (org.nd4j.linalg.exception.ND4JIllegalStateException e) {
+              // モデル内にコミットメッセージが存在しなかったとき
+              log.info(String.format("model does not have the commit message. IngredientA: %s, IngredientB: %s",
+                  ingredientA.commitMessage, ingredientB.commitMessage));
+            }
+            return 0; // default
+          }
+        });
+        // end sort
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
 
-			// We reintroduce the sorted list ingredient into the space
-			this.ingredientSpace.setIngredients(modificationPoint.getCodeElement(), baseElements);
-		}
+      // We reintroduce the sorted list ingredient into the space
+      this.ingredientSpace.setIngredients(modificationPoint.getCodeElement(), baseElements);
+    }
 
-		int size = baseElements.size();
-		if (size > 0) {
-			// We get the smaller element
-			Ingredient ingredient = baseElements.get(0);
-			// we remove it from space
-			baseElements.remove(0);
-			return ingredient;
-		} // any ingredient
-		return null;
-	}
+    int size = baseElements.size();
+    if (size > 0) {
+      // We get the smaller element
+      Ingredient ingredient = baseElements.get(0);
+      // we remove it from space
+      baseElements.remove(0);
+      return ingredient;
+    } // any ingredient
+    return null;
+  }
 
-	/**
-	 * ingredientのリストを探索範囲からとってくる
-	 */
-	public List<Ingredient> getIngredientsFromSpace(ModificationPoint modificationPoint, AstorOperator operationType) {
+  /**
+   * ingredientのリストを探索範囲からとってくる
+   */
+  public List<Ingredient> getIngredientsFromSpace(ModificationPoint modificationPoint, AstorOperator operationType) {
 
-		String type = null;
-		if (operationType instanceof ReplaceOp) {
-			type = modificationPoint.getCodeElement().getClass().getSimpleName();
-		}
+    String type = null;
+    if (operationType instanceof ReplaceOp) {
+      type = modificationPoint.getCodeElement().getClass().getSimpleName();
+    }
 
-		List<Ingredient> elements = null;
-		if (type == null) {
-			elements = this.ingredientSpace.getIngredients(modificationPoint.getCodeElement());
+    List<Ingredient> elements = null;
+    if (type == null) {
+      elements = this.ingredientSpace.getIngredients(modificationPoint.getCodeElement());
 
-		} else {
-			elements = this.ingredientSpace.getIngredients(modificationPoint.getCodeElement(), type);
-		}
+    } else {
+      elements = this.ingredientSpace.getIngredients(modificationPoint.getCodeElement(), type);
+    }
 
-		return elements;
-	}
+    return elements;
+  }
 }
