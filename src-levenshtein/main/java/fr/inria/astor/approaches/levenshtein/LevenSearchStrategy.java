078c29d249b0ed611fb092e3f71ac87522f4b56a
K.Kiyokawa <koichi20110068@gmail.com>
2019-12-07 21:31:58 +0900

    fix import package name (lang -> lang3)

--- src-levenshtein/main/java/fr/inria/astor/approaches/levenshtein/LevenSearchStrategy.java
+++ src-levenshtein/main/java/fr/inria/astor/approaches/levenshtein/LevenSearchStrategy.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.SerializationUtils;
+import org.apache.commons.lang3.SerializationUtils;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.search.spell.LevensteinDistance;
